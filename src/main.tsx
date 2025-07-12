061685e9ec91f9dca8a39fd3c0d27d3e5fd66e11
pvt12 <pvt12@gcloud.ua.es>
2025-07-12 10:48:09 +0200

    Fix - productos Contexto

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import './index.css'
+import { Provider } from 'react-redux';
+import { store } from './slices/MenuItemSlice';
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
